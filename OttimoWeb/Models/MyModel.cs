520274bcb0f5c6882fa3647324dfc33c1a264fe3
Francesco Bullegas <f.bullegas@gmail.com>
2019-12-28 13:39:11 +0100

    Adding a model

---
+++ OttimoWeb/Models/MyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OttimoWeb.Models
+{
+    public class MyModel
+    {
+
+        public string FirstName{get;set;}
+        public string LastName {get;set;}
+    }
+
+    
+}
