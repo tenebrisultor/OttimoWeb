debeed02b60d6181d14fe54a00f87a5b8c5b03b6
Francesco Bullegas <f.bullegas@gmail.com>
2019-12-28 17:41:40 +0100

    Added new Model

---
+++ OttimoWeb/Models/ModelBarbara.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OttimoWeb.Models
+{
+    public class ModelBarbara
+    {
+        public string Nome {get; set;}
+    }
+}
