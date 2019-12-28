38c09b43a314c5b9913ba92076d984b5382e381e
Francesco Bullegas <f.bullegas@gmail.com>
2019-12-28 13:02:17 +0100

    Aggiungere i file di progetto.

---
+++ OttimoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OttimoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
